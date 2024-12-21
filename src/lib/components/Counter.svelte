5a7db9b0d7f2961497fa2136dfebce6de4a0ed6c
Akbar Dzikri <akbardzkr05@gmail.com>
2024-12-21 11:33:21 +0700

    finished the counter component

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let count = $state(0);
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<div class="flex w-fit gap-2 border border-gray-300 p-2">
+	<input
+		type="text"
+		disabled
+		bind:value={count}
+		class="w-32 border border-gray-300 px-4 focus:outline-none"
+	/>
+	<button onclick={increment} class="w-fit border border-gray-300 px-2">Count</button>
+</div>
